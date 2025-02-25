ee2674e74f5dc91d7b056f172cc7120991caa7a5
chinapandaman <benjaminli801@gmail.com>
2025-02-24 22:46:47 -0600

    keep spell

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+mkdir -p ~/.vim/spell/
+mv ~/.vim/spell/ ./spell/
 rm -rf ~/.vim/
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cp coc-settings.json ~/.vim/coc-settings.json
@@ -7,3 +9,4 @@ mkdir ~/.vim/tmp/
 cat plugins.vimrc .vimrc git.vimrc grep.vimrc coc.vimrc > temp
 cp temp ~/.vimrc
 rm temp
+mv ./spell/ ~/.vim/spell/
